dd3e656b07955fc7505e99c246e786588626c1e4
dmg59912 <dmg138380@yahoo.com>
2021-07-12 23:00:17 -0700

    done

--- Project1.py
+++ Project1.py
@@ -8,6 +8,9 @@
 # Version: 1.0.0
 
 
+
+
+
 class Graph(object):
     def __init__(self):
         #defining empty dictionary
@@ -19,10 +22,12 @@ class Graph(object):
         self.vert_dict[node] = new_v
         self.num_vertices = self.num_vertices + 1
 
-
-
+    #returns node value if it is in graph
     def get_vertex(self,node):
-        pass
+        if node not in self.vert_dict:
+            print(node, "not in graph")
+        else:
+            return node
 
     def add_edge(self, from_edge, to_edge, weight):
         if from_edge not in self.vert_dict:
@@ -32,13 +37,20 @@ class Graph(object):
             self.add_vertex(to_edge)
 
         self.vert_dict[from_edge].add_neighbour(self.vert_dict[to_edge], weight)
-        pass
+      
 
     def get_vertices(self):
         return self.vert_dict.keys()
 
-    def graph_summary():
-        pass
+    #plotting our graph 
+    def graph_summary(self):
+        for i in self.get_vertices():
+            node = self.vert_dict[i]
+            for j in node.get_connections():
+                #j_str = str(j)
+               # print(str(node.id))
+                print(i, '->', j.get_id( ) , ' : ', node.get_weight(j))
+
 
 
 class Vertex:
@@ -47,21 +59,53 @@ class Vertex:
         self.id = node
         self.adjacent = {}
 
+    #returns  all connections is our adjcent dictionary
     def get_connections(self):
-        pass
+        return self.adjacent.keys()
 
     def add_neighbour(self, obj_neighbour, weight):
         self.adjacent[obj_neighbour] = weight
 
     def get_id(self):
-        pass
+        return self.id
 
-    def get_weight(self, neigjbour):
-        pass
+    #gets weight of selected neighbour
+    def get_weight(self, neighbour):
+        return self.adjacent[neighbour]
+
 
 #########################################################################################
 def main():
     print("Setting up basic graph")
+
+    graph = Graph()
+
+    #setting up vertex 
+    graph.add_vertex('s')
+    graph.add_vertex('a')
+    graph.add_vertex('b')
+    graph.add_vertex('c')
+    graph.add_vertex('d')
+    
+    #test 
+    #should print vertext not in graph
+    graph.get_vertex('r')
+    
+    #adding edges with weights, From -> TO and weight value
+    graph.add_edge('s','a',10)
+    graph.add_edge('s','c',5)
+    graph.add_edge('a','c',2)
+    graph.add_edge('a','b',1)
+    graph.add_edge('b','d',4)
+    graph.add_edge('c','a',3)
+    graph.add_edge('c','b',9)
+    graph.add_edge('c','d',2)
+    graph.add_edge('d','b',6)
+    graph.add_edge('d','s',7)
+
+
+
+    graph.graph_summary()
 
 
 ###############################################
