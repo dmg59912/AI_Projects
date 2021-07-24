1d451c08ff753995b07b588bb21b62002046c921
dmg59912 <dmg138380@yahoo.com>
2021-07-24 02:30:27 -0700

    Done

--- assignment2/assignment2.py
+++ assignment2/assignment2.py
@@ -1,11 +1,19 @@
+# Assigment 2 
+# Date 7/23/21
+# Rios Rios
+# Student ID:006666515
+# Version 1.0
+
+
 import collections
+from collections import deque
 from os import path
 import queue
-
 
 #global flags to set our start and goal position on our game board 
 goal = -2
 start = -1
+dfs_path = []
 
 ###################################################################################################
 ################# Graph and vertex classes
@@ -36,9 +44,12 @@ class Graph(object):
 
         self.vert_dict[from_edge].add_neighbour(self.vert_dict[to_edge], weight)
       
-
+                
     def get_vertices(self):
         return self.vert_dict.keys()
+
+    def get_num_vertecies(self):
+        return self.num_vertices
 
     def num_vert(self):
         return self.num_vertices
@@ -56,10 +67,12 @@ class Graph(object):
 
 class Vertex:
 
-    def __init__(self, node):
+    def __init__(self,node):
         self.id = node
         self.adjacent = {}
 
+    def __str__(self):
+        return str(self.id)
     #returns  all connections is our adjcent dictionary
     def get_connections(self):
         return self.adjacent.keys()
@@ -70,10 +83,12 @@ class Vertex:
     def get_id(self):
         return self.id
 
-
     #gets weight of selected neighbour
     def get_weight(self, neighbour):
         return self.adjacent[neighbour]
+        
+    def get_location(self):
+        return self.row_location, self.col_location
 
 
 
@@ -83,9 +98,11 @@ class Vertex:
 class BFS(object):
     def __init__(self, graph):
         self.graph = graph
-        self.path = []
-        self.possible_paths = []
+        self.path = deque()
+        self.fringe = 0
 
+    def get_fringe(self):
+        return self.fringe
 
     def bfs_util(self, start, goal, visited, add_path):
 
@@ -93,14 +110,15 @@ class BFS(object):
         visited[start] = True
         self.path.append(start)
 
-        # if current path is the same same our goal
+        # if current vertex is the same same our goal
         if start == goal:
             add_path.append(self.path.copy())
         else:
-            vertexes = self.graph.get_vertex(start)
-            for x in vertexes.adjacent:
-                if visited[x.id] == False:
-                    self.bfs_util(x.id,goal,visited,add_path)
+            vertecies = self.graph.get_vertex(start)
+            for i in vertecies.adjacent:
+                self.fringe += 1
+                if visited[i.id] == False:
+                    self.bfs_util(i.id,goal,visited,add_path)
         
         # remove current vertex from path and marked it as unvisited 
         self.path.pop()
@@ -108,53 +126,97 @@ class BFS(object):
         return add_path
    
     def BFS_paths(self,start,goal):
-        self.add_path = []
+        # create an array to store possible paths
+        self.add_path = deque()
+        # mark all verices as not visited 
         visited = [False] * (self.graph.num_vert() + 1)
+        # recursively call helper function 
         self.add_path = self.bfs_util(start,goal,visited,self.add_path)
         return  self.add_path
 
 ########################################################################################
 
-def DFS_util(self, root):
-    for node_item in root.children_list:
-        if node_item.visited == False:
-            self.DFS_util(node_item)
-            node_item.visited = True
 
-    print(root.get_id())
-    return
+class DFS(object):
+    # counts the max fringe value
+    def __init__(self, graph):
+        self.graph = graph
+        self.fringe = 0
 
-def DFS_traversal(self):
-        self.DFS_util(self.root)
+    def get_fringe(self):
+        return self.fringe
+
+    def dfs_util(self,source, visited, path):
+        #going to mark everynode visited
+        visited.add(source)
+
+        #checking that we have not met our goal 
+        if source.get_id() == start:
+            return True
+
+        # cheking neigbors so we can traverse
+        for i in source.get_connections():
+            self.fringe += 1
+            if i not in visited:
+                path.put(i)
+                found = self.dfs_util(i, visited, path)
+                #we will pop form the LIFO untiol we meet our goal hence the else
+                if found:
+                    return True
+                else:  
+                    path.get()
+        
+        return False
+
+    def dfs_path(self):
+        p_node = self.graph.get_vertex(goal)
+
+        # to mark our visited vertex/nodes
+        visited = set()
+
+        # path to goal (LIFO Queue)
+        path = queue.LifoQueue()
+
+        # recursively call helper function 
+        self.dfs_util(p_node, visited, path)
+    
+        #pass our path to our golabl dfs_path list
+        while not path.empty():
+            vertex = path.get()
+            dfs_path.append(vertex.get_id())
+        dfs_path.append(goal)
+ 
 
 ##############################################################################################
 ########################## main #######################################
 def main():
     print("Setting up basic maze")
 
-    #setting up graph and files 
-    
-    #file_to_open = "bigMaze.lay"
-    file_to_open = "mediumMaze.lay"
-    #file_to_open = "smallMaze.lay"
+    #setting up graph and files remove '#' and add '#'  to chose which file to run
+    ######################################## OUR FILES TO READ ###########################
+    file_to_open = "bigMaze.lay"                 #
+    #file_to_open = "mediumMaze.lay"              #  
+    #file_to_open = "smallMaze.lay"                #
+    ######################################################
     
     graph = Graph()
+    graphDfs = Graph()
 
 
-    #adding to a file to matrix chars
+    #adding to a file to matrix chars for BFS
     game_board = []
     f = open(file_to_open,"r")
     line = f.readline()
     while line:
         col = []
         for x in line:
-            print(x,end='')
+            #print(x,end='')
             if x != '\n':
                 col.append(x)
         line = f.readline() 
         game_board.append(col)
 
-
+    f.close()
     new_b = game_board
     # count will increment to set vertex values if there is an open space 
     count = 0
@@ -178,11 +240,6 @@ def main():
                 graph.add_vertex(start)
                 
 
-    #print(graph.get_vertices())
-     # printing board 
-  
-    print('\n\n','Total vertecies',count)
- 
     # setting up travel directions to travel on our matrix board 
     move_up = -1
     move_right = 1
@@ -223,12 +280,107 @@ def main():
                         graph.add_edge(game_board[i][j], game_board[i + move_up][j], 1)
             #print(i,j)
             #check top and top left  boundaries are in range
+    #######################################################################################################
+    ##########################################################################################################
+    ### setting game board for DFS 
+    game_board_dfs = []
+    f = open(file_to_open,"r")
+    line = f.readline()
+    while line:
+        col = []
+        for x in line:
+            #print(x,end='')
+            if x != '\n':
+                col.append(x)
+        line = f.readline() 
+        game_board_dfs.append(col)
 
-   
+    f.close() 
+    new_d = game_board_dfs
+    # count will increment to set vertex values if there is an open space 
+    count = 0
 
+ 
+    # Addinng nodes to our graph
+    for i in range(len(game_board_dfs)):
+        for j in range(len(game_board_dfs[0])):
+            #print(i,j, '  ', end='')
+            if(game_board_dfs[i][j] == ' '):
+                count = count + 1
+                new_d[i][j] = count
+                graphDfs.add_vertex(count)
+            elif(game_board_dfs[i][j] == 'P'):
+                count = count +1
+                new_d[i][j] = goal
+                graphDfs.add_vertex(goal)
+            elif(game_board_dfs[i][j] == '.'):
+                count = count + 1
+                new_d[i][j] = start
+                graphDfs.add_vertex(start)
+
+    #now adding connections between our verticies 
+    for i in range(len(game_board_dfs)):
+        for j in range(len(game_board_dfs[0])):
+            # check that we are not outside right boundary if not, then add connection to node if any
+            if( j + move_right) != right_boundary:
+               if game_board_dfs[i][j] != '%':
+                    if(game_board_dfs[i][j + move_right] != '%'):
+                        graphDfs.add_edge(game_board_dfs[i][j], game_board_dfs[i][j + move_right], 1)
+
+            # check that we are not outside botton boundary, if not add connection to node if any 
+            if(i + move_down) != botton_boundary:
+               if game_board_dfs[i][j] != '%':
+                    if(game_board_dfs[i + move_down][j] != '%'):
+                        graphDfs.add_edge(game_board_dfs[i][j], game_board_dfs[i + move_down][j], 1)
+            # check that we are not outside our left boundary, if not add connection to node if any
+            if(j + move_left != left_boundary):
+               if game_board_dfs[i][j] != '%':
+                    if(game_board_dfs[i][j + move_left] != '%'):
+                        graphDfs.add_edge(game_board_dfs[i][j], game_board_dfs[i][j + move_left], 1)
+
+            # check that we are not outside our top boundary, if not add connection to node if any
+            if(i + move_up != top_boundary):
+                if game_board_dfs[i][j] != '%':
+                    if(game_board_dfs[i + move_up][j] != '%'):
+                        graphDfs.add_edge(game_board_dfs[i][j], game_board_dfs[i + move_up][j], 1)
+            #print(i,j)
+            #check top and top left  boundaries are in range
+
+
+    dfs = DFS(graphDfs)
+    dfs_shortes = dfs.dfs_path()
+
+    for i in range(len(game_board_dfs)):
+        for j in range(len(game_board_dfs[0])):
+            for k in dfs_path:
+                if game_board_dfs[i][j] == k:
+                    game_board_dfs[i][j] = '.'
+                elif game_board_dfs[i][j] == goal:
+                    game_board_dfs[i][j] = 'P'
+
+    # remove vertex ids on our map and set it to only show current shortes path 
+    for i in range(len(game_board_dfs)):
+        for j in range(len(game_board_dfs[0])):
+            if (game_board_dfs [i][j] != '%') and ((game_board_dfs[i][j] != '.') and (game_board_dfs[i][j] != 'P') ):
+               game_board_dfs[i][j] = ' '
+            print(game_board_dfs[i][j], end ="")
+        print()
+
+
+    print()
+    print('Shortes path cost',len(dfs_path)- 1)
+    print('Path taken with -1 = start and -2 = goal=', dfs_path)
+    print('Total nodes visited/expanded =',graphDfs.get_num_vertecies())
+    print('Max Fringe ', dfs.get_fringe())
+
+
+    ####################################################################################
+    #####################################################################################
+    #setting BFS 
 
     bfs = BFS(graph)
-    shortes = bfs.BFS_paths(start,goal)
+    bfs_shortes = bfs.BFS_paths(start,goal)
+
 
     # need to find the shortes path from the list of paths found 
 
@@ -236,15 +388,14 @@ def main():
     #give flag value for our current path to conpare and set the path with the lowest travel distance per say
     current_path = 10000000
     shortes_path_index = 0
-    for i in range(len(shortes)):
-        for j in range(len(shortes[i])):
+    for i in range(len(bfs_shortes)):
+        for j in range(len(bfs_shortes[i])):
             path_count += 1
         if current_path > path_count:
             current_path = path_count - 1
             shortes_path_index = i
         path_count = 0
-
-
+    # need to find the shortes path from the list of paths found 
 
        
            
@@ -252,7 +403,7 @@ def main():
     # map our shortes path with '.' 
     for i in range(len(game_board)):
         for j in range(len(game_board[0])):
-            for k in shortes[shortes_path_index]:
+            for k in bfs_shortes[shortes_path_index]:
                 if game_board[i][j] == k:
                     game_board[i][j] = '.'
                 elif game_board[i][j] == goal:
@@ -266,7 +417,11 @@ def main():
             print(game_board[i][j], end ="")
         print()
     
+    print()
     print('Shortes path cost',current_path)
+    print('Path taken with -1 = start and -2 = goal=', bfs_shortes[shortes_path_index])
+    print('Total nodes visited/expanded =',graph.get_num_vertecies())
+    print('Max Fringe ', bfs.get_fringe())
 
        
            
